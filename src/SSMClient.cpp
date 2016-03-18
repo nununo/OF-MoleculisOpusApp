acd626e0e180bce470020edec5425db7b0050d2f
Nuno Godinho <mail@nunogodinho.com>
2016-03-18 19:25:07 +0000

    gSOAP files added and compiling successfully

---
+++ src/SSMClient.cpp
@@ -0,0 +1,9 @@
+//
+//  SSMClient.cpp
+//  MoleculisOpusApp
+//
+//  Created by Nuno on 18/03/16.
+//
+//
+
+#include "SSMClient.hpp"
---
+++ src/SSMClient.hpp
@@ -0,0 +1,17 @@
+//
+//  SSMClient.hpp
+//  MoleculisOpusApp
+//
+//  Created by Nuno on 18/03/16.
+//
+//
+
+#ifndef SSMClient_hpp
+#define SSMClient_hpp
+
+#include <stdio.h>
+
+#include "gSOAP/soapzssm_USCORElistmessagesProxy.h"
+#include "gSOAP/zssm_USCORElistmessages.nsmap"
+
+#endif /* SSMClient_hpp */
