df383f245f05bfafcd52f8ff5dc915fce5381018
Nuno Godinho <mail@nunogodinho.com>
2016-04-29 16:34:31 +0100

    Restarting slowing to see what I got wrong the last time

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -3,7 +3,6 @@
 #include "gSOAP/soapzssm_USCORElistmessagesProxy.h"
 #include "gSOAP/zssm_USCORElistmessages.nsmap"
 
-
 //--------------------------------------------------------------
 void ofApp::setup(){
   
@@ -11,62 +10,43 @@ void ofApp::setup(){
 
 //--------------------------------------------------------------
 void ofApp::update(){
-<<<<<<< HEAD
- zssm_USCORElistmessagesProxy ssmProxy;
-
-=======
   zssm_USCORElistmessagesProxy ssmProxy;
-    _ns1__Z_USCORESSM_USCORELIST_USCOREMESSAGES *listMessages = new _ns1__Z_USCORESSM_USCORELIST_USCOREMESSAGES();
-  _ns1__Z_USCORESSM_USCORELIST_USCOREMESSAGESResponse response;
-
-  // Request timeout
->>>>>>> parent of f994d2f... Mysterious linking error
-  ssmProxy.soap->connect_timeout = 15; // connect within 10s
-  ssmProxy.soap->send_timeout = 5;     // send timeout is 5s
-  ssmProxy.soap->recv_timeout = 10;     // receive timeout is 5s
-
-<<<<<<< HEAD
-//  ssmProxy.soap->user = @"OUTSYSTEMS";
-//  ssmProxy.soap->password = @"systemout";
   
-//  char myCharString[10] = "mystring";
-//  ssmProxy.soap->user = [[NSString alloc] initWithUTF8String:myCharString]; http://www.dreamincode.net/forums/topic/223434-convert-charstring-to-nsstring/
-
   _ns1__Z_USCORESSM_USCORELIST_USCOREMESSAGES *listMessages = new _ns1__Z_USCORESSM_USCORELIST_USCOREMESSAGES();
   _ns1__Z_USCORESSM_USCORELIST_USCOREMESSAGESResponse response;
   
-=======
+  // Request timeout
+  ssmProxy.soap->connect_timeout = 15; // connect within 10s
+  ssmProxy.soap->send_timeout = 5;     // send timeout is 5s
+  ssmProxy.soap->recv_timeout = 10;     // receive timeout is 5s
+  
   // HTTP Authentication
   ssmProxy.soap->userid = "OUTSYSTEMS";
   ssmProxy.soap->passwd = "systemsout";
   
   // Input parameters
-  listMessages->ALL = "0";
-
+  //listMessages->ALL = "0";
+  listMessages->DATA = "2016-04-21";
+  
   // SOAP call
->>>>>>> parent of f994d2f... Mysterious linking error
   if ( ssmProxy.Z_USCORESSM_USCORELIST_USCOREMESSAGES(listMessages, response) == SOAP_OK )
     std::cout << "OK" << std::endl;
   else
     ssmProxy.soap_stream_fault(std::cerr);
   ssmProxy.destroy();
-<<<<<<< HEAD
-=======
-
->>>>>>> parent of f994d2f... Mysterious linking error
 }
 
 //--------------------------------------------------------------
 void ofApp::draw(){
-
+  
 }
 
 //--------------------------------------------------------------
 void ofApp::keyPressed(int key){
-
+  
 }
 
 //--------------------------------------------------------------
 void ofApp::windowResized(int w, int h){
-
+  
 }
