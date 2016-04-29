a1e0a587e9ee84a13492379d620bf192fdc3aae4
Nuno Godinho <mail@nunogodinho.com>
2016-04-29 15:32:31 +0100

    Revert "HTTP Authentication sorted out"
    This reverts commit 5c9dc5d2282923f819786d0fb194415c04a8b4bb.

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -7,7 +7,26 @@ void ofApp::setup(){
 
 //--------------------------------------------------------------
 void ofApp::update(){
-  ssmClient->fetch();
+ zssm_USCORElistmessagesProxy ssmProxy;
+
+  ssmProxy.soap->connect_timeout = 15; // connect within 10s
+  ssmProxy.soap->send_timeout = 5;     // send timeout is 5s
+  ssmProxy.soap->recv_timeout = 10;     // receive timeout is 5s
+
+//  ssmProxy.soap->user = @"OUTSYSTEMS";
+//  ssmProxy.soap->password = @"systemout";
+  
+//  char myCharString[10] = "mystring";
+//  ssmProxy.soap->user = [[NSString alloc] initWithUTF8String:myCharString]; http://www.dreamincode.net/forums/topic/223434-convert-charstring-to-nsstring/
+
+  _ns1__Z_USCORESSM_USCORELIST_USCOREMESSAGES *listMessages = new _ns1__Z_USCORESSM_USCORELIST_USCOREMESSAGES();
+  _ns1__Z_USCORESSM_USCORELIST_USCOREMESSAGESResponse response;
+  
+  if ( ssmProxy.Z_USCORESSM_USCORELIST_USCOREMESSAGES(listMessages, response) == SOAP_OK )
+    std::cout << "OK" << std::endl;
+  else
+    ssmProxy.soap_stream_fault(std::cerr);
+  ssmProxy.destroy();
 }
 
 //--------------------------------------------------------------
